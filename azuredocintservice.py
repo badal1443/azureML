cbb3429200f942cf0c79a0727d9b75825ca5edfa
Badal Singh <badal1443@gmail.com>
2024-10-03 05:03:53 +0000

    Tested code

--- azuredocintservice.py
+++ azuredocintservice.py
@@ -1,16 +1,24 @@
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.documentintelligence import DocumentIntelligenceClient
-from azure.ai.documentintelligence.models import AnalyzeResult
+from azure.ai.formrecognizer import DocumentAnalysisClient
+import os
 
-endpoint = "<your-endpoint>"
-key = "<your-key>"
+endpoint=os.environ["ENDPOINT_URL"]
+key=os.environ["ENDPOINT_KEY"]
 
-docUrl = "<url-of-document-to-analyze>"
 
-document_analysis_client = DocumentIntelligenceClient(endpoint=endpoint, 
-    credential=AzureKeyCredential(key))
+url = "https://raw.githubusercontent.com/Azure/azure-sdk-for-python/main/sdk/formrecognizer/azure-ai-formrecognizer/tests/sample_forms/receipt/contoso-receipt.png"
 
-poller = document_analysis_client.begin_analyze_document(
-        "prebuilt-layout", AnalyzeDocumentRequest(url_source=docUrl
-    ))
-result: AnalyzeResult = poller.result()
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-receipt", url)
+receipts = poller.result()
+
+## Retrieve items
+
+for idx, receipt in enumerate(receipts.documents):
+    merchant_name = receipt.fields.get("MerchantName")
+    date_of_buy = receipt.fields.get("TransactionDate")
+    print(merchant_name.value)
+    print(date_of_buy.value)
